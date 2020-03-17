62d8ddb432e74f5381fb44df3e6c73b6072883d6
codershona <falgunimst95@gmail.com>
2020-03-17 16:07:31 +0100

    project structure created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-// import Blog from './containers/Blog/Blog';
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div>
-       <p> TEST </p>
+      <div className="App">
+        <Blog />
       </div>
     );
   }
