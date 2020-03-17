42864af2aff93cfd546a4f576fe2f36631583321
codershona <falgunimst95@gmail.com>
2020-03-17 17:45:06 +0100

    Making a Post Selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
    state = {
-      posts: []
+      posts: [],
+      selectedPostId: null
    }
    
 
@@ -30,10 +31,18 @@ class Blog extends Component {
 
  }
    
+   postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id});
+
+   }
   
     render () {
       const posts = this.state.posts.map(post => {
-        return <Post key={post.id} title={post.title} author={post.author} />;
+        return <Post key={post.id} 
+        title={post.title} 
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)} 
+        />;
 
       });
 
@@ -46,7 +55,7 @@ class Blog extends Component {
 
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
