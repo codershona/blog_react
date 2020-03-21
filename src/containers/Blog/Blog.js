e42695ea0583caa31cee3d2973ae8c5523d2f95d
codershona <falgunimst95@gmail.com>
2020-03-21 20:07:28 +0100

    react commented some code

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -25,8 +25,8 @@ class Blog extends Component {
                   </nav>
                 </header>
 
-               <Route path="/" exact render={() => <h1>Home</h1>} />
-          <Route path="/" render={() => <h1>Home 2</h1>} />
+             {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+          <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
             </div>
         );
