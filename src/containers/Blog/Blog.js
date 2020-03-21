adc775f23b23287b3d3c69275ad4bcabf1582bd1
codershona <falgunimst95@gmail.com>
2020-03-21 20:11:06 +0100

    Rendering Components for Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -27,6 +27,8 @@ class Blog extends Component {
 
              {/* <Route path="/" exact render={() => <h1>Home</h1>} />
           <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+          <Route path="/" exact component={Posts} />
+
 
             </div>
         );
