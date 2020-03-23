838976c435029cc1860aef4ce31776e1d21e8084
codershona <falgunimst95@gmail.com>
2020-03-23 18:10:56 +0100

    Creating Dynamic Nested Routes done

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -25,7 +25,7 @@ class FullPost extends Component {
   loadData () {
 
         if ( this.props.match.params.id ) {
-            if ( !this.state.loadedPosts || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+            if ( !this.state.loadedPosts || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id) ) {
           
  axios.get( '/posts/' + this.props.match.params.id )
    .then(response => {
@@ -44,7 +44,7 @@ class FullPost extends Component {
 
 
   deletePostHandler = () => {
-   axios.delete('/posts/' + this.props.id)
+   axios.delete('/posts/' + this.props.match.params.id)
       .then(response => {
         console.log(response);
       });
@@ -54,7 +54,7 @@ class FullPost extends Component {
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
      
-      if (this.props.id) {
+      if (this.props.match.params.id) {
         post = <p style={{textAlign: 'center'}}>Loading......!</p>;
       }
 
